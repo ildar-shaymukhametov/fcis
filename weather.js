77ed10c421cca75577b0cafce31d88fd027c1197
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-18 14:57:01 +0500

    reformat

--- weather.js
+++ weather.js
@@ -1,14 +1,18 @@
 // Get weather from external REST API and store it in a file
-const { get_weather } = require('./openweather-adaptor')
-const { write_file } = require('./file-adaptor')
+const { get_weather } = require("./openweather-adaptor");
+const { write_file } = require("./file-adaptor");
 
 // Secrets
-const API_KEY = process.env['API_KEY'] || 'fail'
+const API_KEY = process.env["API_KEY"] || "fail";
 
 module.exports = {
-  main: async function(context, weather_service = get_weather, file_service = write_file) {
-    const city = getCity(context)
-    console.log('getting weather for city', city)
+  main: async function (
+    context,
+    weather_service = get_weather,
+    file_service = write_file
+  ) {
+    const city = getCity(context);
+    console.log("getting weather for city", city);
 
     const data = await getData(city);
     if (data instanceof Error) {
@@ -18,23 +22,23 @@ module.exports = {
 
     writeToFile(file_service, getFilename(), data);
 
-    return (city)
-  }
-}
+    return city;
+  },
+};
 function writeToFile(file_service, filename, data) {
-  const result = file_service(filename, data)
-  if (result.status == 'success') {
-    console.log('wrote file', result.file_written)
+  const result = file_service(filename, data);
+  if (result.status == "success") {
+    console.log("wrote file", result.file_written);
   } else {
-    console.log('error writing file', result.error)
+    console.log("error writing file", result.error);
   }
 }
 
 function getFilename() {
-  let today = new Date()
-  today = today.toISOString().slice(0, 10)
-  const filename = 'weather-' + today + '.json'
-  return filename
+  let today = new Date();
+  today = today.toISOString().slice(0, 10);
+  const filename = "weather-" + today + ".json";
+  return filename;
 }
 
 async function getData(city) {
@@ -47,12 +51,11 @@ async function getData(city) {
   }
 }
 function getCity(context) {
-  let city = ''
+  let city = "";
   if (context && context?.city) {
-    city = context.city
+    city = context.city;
   } else {
-    city = 'london'
+    city = "london";
   }
-  return city
+  return city;
 }
-
